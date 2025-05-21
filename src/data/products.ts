edebea9ff9ae3c0d7b25f9e7dd56c299105e3407
Jaidevpatel25 <jaidevp900@gmail.com>
2025-05-21 13:16:55 +0530

    Update products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -102,7 +102,7 @@ export const products: Product[] = [
     id: "motorola-edge-50-fusion",
     name: "Edge 50 Fusion",
     brand: "Motorola",
-    price: 25000.00,
+    price: 25,000.00,
     image: "https://motorolain.vtexassets.com/arquivos/ids/159186/2024_CUSCO-PLUS_FOREST-GREEN_PDP-HERO.png?v=638618115763230000",
     specs: {
       ram: "12GB",
